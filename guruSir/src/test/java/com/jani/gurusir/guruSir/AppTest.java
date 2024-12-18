c0e1bdcafeaf0ea79b9cc31979d868ba675902d9
unknown <patanjanibasha1@gmail.com>
2024-12-18 13:36:03 +0530

    push java code

---
+++ guruSir/src/test/java/com/jani/gurusir/guruSir/AppTest.java
@@ -0,0 +1,20 @@
+package com.jani.gurusir.guruSir;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+/**
+ * Unit test for simple App.
+ */
+public class AppTest { 
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {	
+        assertTrue(true);
+    }
+}
